4670863c9d142b3d3a36e806c6cd15e1cb52e5ed
Leticia <leticia.fraga@epitech.eu>
2024-03-29 18:12:27 +0100

    test: fix tests

--- tests/test_find_seps.c
+++ tests/test_find_seps.c
@@ -23,26 +23,21 @@ Test(find_seps_test, basic_test)
 
 Test(find_seps_test, many_reds_test)
 {
-    char line[] = "ls -l; ls -l | wc -l >> cat file";
+    char line[] = "ls -l | wc -l >> cat file";
     redirection_map *r = find_seps(line);
     redirection_opts *opts;
 
-    cr_assert_eq(r->cnt, 4);
+    cr_assert_eq(r->cnt, 3);
 
     opts = r->arr[0];
-    cr_assert_eq(opts->symbol, semicolon);
-    cr_assert_eq(opts->prev_cmd_end, 4);
-    cr_assert_eq(opts->next_cmd_index, 6);
+    cr_assert_eq(opts->symbol, pipe_symbol);
+    cr_assert_eq(opts->prev_cmd_end, 5);
+    cr_assert_eq(opts->next_cmd_index, 7);
 
     opts = r->arr[1];
-    cr_assert_eq(opts->symbol, pipe_symbol);
-    cr_assert_eq(opts->prev_cmd_end, 12);
-    cr_assert_eq(opts->next_cmd_index, 14);
-
-    opts = r->arr[2];
     cr_assert_eq(opts->symbol, out2);
-    cr_assert_eq(opts->prev_cmd_end, 20);
-    cr_assert_eq(opts->next_cmd_index, 23);
+    cr_assert_eq(opts->prev_cmd_end, 13);
+    cr_assert_eq(opts->next_cmd_index, 16);
 
     free_seps(r);   
 }
@@ -75,26 +70,21 @@ Test(find_seps_test, in_out_test)
 
 Test(find_seps_test, different_spacing)
 {
-    char line[] = "ls -l ; ls -l|wc -l >>cat file";
+    char line[] = " ls -l|wc -l >>cat file";
     redirection_map *r = find_seps(line);
     redirection_opts *opts;
 
-    cr_assert_eq(r->cnt, 4);
+    cr_assert_eq(r->cnt, 3);
 
     opts = r->arr[0];
-    cr_assert_eq(opts->symbol, semicolon);
+    cr_assert_eq(opts->symbol, pipe_symbol);
     cr_assert_eq(opts->prev_cmd_end, 5);
     cr_assert_eq(opts->next_cmd_index, 7);
 
     opts = r->arr[1];
-    cr_assert_eq(opts->symbol, pipe_symbol);
-    cr_assert_eq(opts->prev_cmd_end, 12);
-    cr_assert_eq(opts->next_cmd_index, 14);
-
-    opts = r->arr[2];
     cr_assert_eq(opts->symbol, out2);
-    cr_assert_eq(opts->prev_cmd_end, 19);
-    cr_assert_eq(opts->next_cmd_index, 22);
+    cr_assert_eq(opts->prev_cmd_end, 12);
+    cr_assert_eq(opts->next_cmd_index, 15);
 
     free_seps(r);   
 }
